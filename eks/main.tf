43fdd6b42e8facb7561357e5f61b22a8fcd1ddc1
lyj2174 <lyj2174@gmail.com>
2025-12-11 09:11:59 +0900

    vpc setting changed

--- eks/main.tf
+++ eks/main.tf
@@ -8,15 +8,17 @@ module "eks" {
   vpc_id     = var.vpc_id
   subnet_ids = var.private_subnets
 
-  # kubectl 로컬 접근 허용
   cluster_endpoint_public_access = true
 
-  # -------------------------------------------------------
-  # 노드그룹 2개 구성
-  # -------------------------------------------------------
-  eks_managed_node_groups = {
+  enable_irsa = true
 
-    # FE / MQTT 노드그룹
+  cluster_addons = {
+    coredns   = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni   = { most_recent = true }
+  }
+
+  eks_managed_node_groups = {
     fe_group = {
       node_group_name = "fe-mqtt-nodegroup"
       instance_types  = ["t3.small"]
@@ -24,17 +26,9 @@ module "eks" {
       min_size        = 1
       max_size        = 2
       capacity_type   = "ON_DEMAND"
-
-      labels = {
-        role = "frontend"
-      }
-
-      tags = {
-        "Name" = "sf-node-fe"
-      }
+      labels = { role = "frontend" }
     }
 
-    # BE 노드그룹
     be_group = {
       node_group_name = "backend-nodegroup"
       instance_types  = ["t3.small"]
@@ -42,16 +36,63 @@ module "eks" {
       min_size        = 1
       max_size        = 2
       capacity_type   = "ON_DEMAND"
-
-      labels = {
-        role = "backend"
-      }
-
-      tags = {
-        "Name" = "sf-node-be"
-      }
+      labels = { role = "backend" }
     }
   }
 
   enable_cluster_creator_admin_permissions = true
+}
+
+data "http" "alb_iam_policy" {
+  url = "https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.6.2/docs/install/iam_policy.json"
+}
+
+resource "aws_iam_policy" "alb_controller" {
+  name   = "AWSLoadBalancerControllerIAMPolicy"
+  policy = data.http.alb_iam_policy.body
+}
+
+module "alb_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name = "alb-controller-irsa"
+
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    eks = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
+
+module "aws_lb_controller" {
+  source  = "terraform-aws-modules/helm/aws"
+  version = "~> 1.0"
+
+  cluster_name           = module.eks.cluster_name
+  cluster_endpoint       = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  chart      = "aws-load-balancer-controller"
+  name       = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  repository = "https://aws.github.io/eks-charts"
+
+  values = [
+    yamlencode({
+      clusterName = module.eks.cluster_name
+      region      = var.aws_region
+      vpcId       = var.vpc_id
+      serviceAccount = {
+        create = true
+        name   = "aws-load-balancer-controller"
+        annotations = {
+          "eks.amazonaws.com/role-arn" = module.alb_irsa.iam_role_arn
+        }
+      }
+    })
+  ]
 }
--- eks/terraform.tfvars
+++ eks/terraform.tfvars
@@ -1,10 +1,10 @@
 cluster_name = "smart-eks"
 
-vpc_id = "vpc-044da7bca7878d0c0"
+vpc_id = "vpc-04ab060289866dffb"
 
 private_subnets = [
-  "subnet-0731f398a458c8b30",
-  "subnet-0a2d9f07c4af15fda"
+  "subnet-0f9f475e41e22d94b",
+  "subnet-0eaf8dbc828c3501c"
 ]
 
 aws_region = "ap-northeast-2"
