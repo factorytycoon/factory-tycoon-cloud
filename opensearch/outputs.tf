1d5c373dc21caa91b05bbaa7f9e1f0c5310dfb4b
jiminu <jmu417@gmail.com>
2025-12-12 13:44:58 +0900

    add: opensearch

---
+++ opensearch/outputs.tf
@@ -0,0 +1,34 @@
+output "domain_id" {
+  description = "OpenSearch 도메인 ID"
+  value       = aws_opensearch_domain.main.domain_id
+}
+
+output "domain_arn" {
+  description = "OpenSearch 도메인 ARN"
+  value       = aws_opensearch_domain.main.arn
+}
+
+output "endpoint" {
+  description = "OpenSearch 도메인 엔드포인트"
+  value       = aws_opensearch_domain.main.endpoint
+}
+
+output "dashboard_endpoint" {
+  description = "OpenSearch Dashboards 엔드포인트"
+  value       = aws_opensearch_domain.main.dashboard_endpoint
+}
+
+output "security_group_id" {
+  description = "OpenSearch 보안 그룹 ID"
+  value       = aws_security_group.opensearch.id
+}
+
+output "access_url" {
+  description = "OpenSearch 접속 URL"
+  value       = "https://${aws_opensearch_domain.main.endpoint}"
+}
+
+output "dashboard_url" {
+  description = "OpenSearch Dashboards 접속 URL"
+  value       = "https://${aws_opensearch_domain.main.endpoint}/_dashboards"
+}
