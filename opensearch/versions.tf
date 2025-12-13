0afb0edaa8d57aef2b731f518b0c5b31e98900e5
jiminu <jmu417@gmail.com>
2025-12-13 13:51:47 +0900

    fix: bucket name

--- opensearch/versions.tf
+++ opensearch/versions.tf
@@ -9,7 +9,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket  = "factory-tycoon-terraform-state1"
+    bucket  = "factory-tycoon-terraform-state"
     key     = "opensearch/terraform.tfstate"
     region  = "ap-northeast-2"
   }
