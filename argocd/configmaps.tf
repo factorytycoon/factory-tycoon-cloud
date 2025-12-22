58fa62b86a71bb707765e6dab67794bd05cf39d2
lyj2174 <lyj2174@gmail.com>
2025-12-22 21:31:15 +0900

    modify argocd secret and cofing about k8s

---
+++ argocd/configmaps.tf
@@ -0,0 +1,25 @@
+resource "kubernetes_config_map" "sf_backend_config" {
+  metadata {
+    name      = "sf-backend-config"
+    namespace = "default"
+  }
+
+  data = {
+    JWT_ACCESS_TTL_MS  = var.jwt_access_ttl_ms
+    JWT_REFRESH_TTL_MS = var.jwt_refresh_ttl_ms
+    AWS_REGION         = var.aws_region
+    REDIS_PORT         = var.backend_redis_port
+  }
+}
+
+resource "kubernetes_config_map" "sf_backend_glb_config" {
+  metadata {
+    name      = "sf-backend-glb-config"
+    namespace = "default"
+  }
+
+  data = {
+    SERVER_PORT = var.glb_server_port
+    AWS_REGION  = var.aws_region
+  }
+}
