22572bb5ef3fd329145aa5dd8bbf0941ed045718
lyj2174 <lyj2174@gmail.com>
2025-12-29 21:08:24 +0900

    Auto insert alb_dns_name

--- cloudfront/variables.tf
+++ cloudfront/variables.tf
@@ -1,4 +1,18 @@
 variable "alb_dns_name" {
   type        = string
   description = "ALB DNS name created by EKS Ingress"
+  default     = null
+  nullable    = true
+}
+
+variable "ingress_name" {
+  type        = string
+  description = "Kubernetes Ingress name that provisions the ALB"
+  default     = "factory-ingress"
+}
+
+variable "ingress_namespace" {
+  type        = string
+  description = "Kubernetes namespace for the Ingress"
+  default     = "default"
 }
