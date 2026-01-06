f1cfad5f037da69b1bcd5f4d635bcbaff93521e3
jiminu <jmu417@gmail.com>
2026-01-06 10:00:38 +0900

    add: opensearch policy

--- eks/irsa-aws.tf
+++ eks/irsa-aws.tf
@@ -1,3 +1,8 @@
+# ---------- Data Sources ----------
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 # ---------- IAM Policy (S3 Read for GLB) ----------
 resource "aws_iam_policy" "aws_s3_read" {
   name = "sf-backend-aws-s3-read"
@@ -40,6 +45,30 @@ resource "aws_iam_policy" "aws_bedrock_access" {
   })
 }
 
+# ---------- IAM Policy (OpenSearch Access) ----------
+resource "aws_iam_policy" "aws_opensearch_access" {
+  name        = "sf-backend-aws-opensearch-access"
+  description = "Allow access to AWS OpenSearch"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          # "es:ESHttpGet",
+          # "es:ESHttpPost",
+          # "es:ESHttpPut",
+          # "es:ESHttpDelete",
+          # "es:ESHttpHead"
+          "es:*"
+        ]
+        Resource = "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/opensearch/*"
+      }
+    ]
+  })
+}
+
 # ---------- IRSA Role ----------
 module "aws_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
@@ -48,8 +77,9 @@ module "aws_irsa" {
   role_name = "sf-backend-aws-irsa"
 
   role_policy_arns = {
-    s3      = aws_iam_policy.aws_s3_read.arn
-    bedrock = aws_iam_policy.aws_bedrock_access.arn
+    s3         = aws_iam_policy.aws_s3_read.arn
+    bedrock    = aws_iam_policy.aws_bedrock_access.arn
+    opensearch = aws_iam_policy.aws_opensearch_access.arn
   }
 
   oidc_providers = {
